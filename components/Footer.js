0d82999704bee04cd6d00bbd8d2d8d92a542bc6b
joealsaaidah <alsaaidahyousef@gmail.com>
2022-01-19 18:49:44 +0800

    real estate application | before deploy

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+const Footer = () => {
+  return (
+    <Box
+      textAlign='center'
+      p='5'
+      color='gray.600'
+      borderTop='1px'
+      borderColor='gray.100'
+    >
+      &copy; 2021 MiMiCuCu-Realtor, Inc.
+    </Box>
+  );
+};
+
+export default Footer;
